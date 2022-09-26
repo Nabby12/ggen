d10c9c957823410bf83e800c8e25ed3ab741f75c
Nabby12 <Nabby12@users.noreply.github.com>
2022-09-26 22:43:18 +0900

    added/script-for-creating-new-version

---
+++ scripts/create-new-version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+latestTag=$(git describe --abbrev=0 --tags)
+cmdStatus=$?
+
+if [ $cmdStatus -ne 0 ]; then
+    echo "v0.0.0"
+    exit 0
+fi
+
+latestTag=$(echo ${latestTag} | awk '{print substr($0, 2)}')
+
+IFS=. tagArray=(${latestTag})
+major=${tagArray[0]}
+minor=${tagArray[1]}
+revision=${tagArray[2]}
+
+if [ ${revision} -ge 9 ]; then
+    if [ ${minor} -ge 9 ]; then
+        major=$((major+1))
+        minor=0
+        revision=0
+    else
+        minor=$((minor+1))
+        revision=0
+    fi
+else
+  revision=$((revision+1))
+fi
+
+echo "v${major}.${minor}.${revision}"
