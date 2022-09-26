cfba753af8a2f92110a0feb99513fba9deaa9056
Nabby12 <Nabby12@users.noreply.github.com>
2022-09-26 20:33:33 +0900

    added/script-for-creating-new-tag

---
+++ scripts/tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+latestTag=$(git describe --abbrev=0 --tags 2>&1 > /dev/null)
+
+if [[ "$latestTag" == *fatal* ]]; then
+    echo "v0.0.0"
+    return 0
+fi
+
+latestTag=$(echo ${latestTag} | awk '{print substr($0, 2)}')
+
+IFS=. tagArray=(${latestTag})
+major=${tagArray[0]}
+minor=${tagArray[1]}
+revision=${tagArray[2]}
+
+if [ ${revision} -ge 9 ]; then
+    if [ ${minor} -ge 9 ]; then
+        major=$((major+1))
+        minor=0
+        revision=0
+    else
+        minor=$((minor+1))
+        revision=0
+    fi
+else
+  revision=$((revision+1))
+fi
+
+echo "v${major}.${minor}.${revision}"
